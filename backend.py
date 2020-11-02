b2719f4f9756b7130b7e0d4020eba67d5135f8ea
Raphael Treccani-Chinelli <raphael.treccanichinelli@gmail.com>
2020-11-02 22:51:05 +0000

    added some more spicy commands

--- backend.py
+++ backend.py
@@ -1,9 +1,12 @@
-import logging, sys, math
+import logging, sys, math, random
 
 #logging.basicConfig(filename='TRPN.log', filemode ='w', level=logging.DEBUG
 
 class TRPNBackend:
     stack = []
+
+    def popFromBOS(self):
+        return(self.stack.pop())
 
     def getLast2Items(self):
         return self.stack.pop(), self.stack.pop()
@@ -23,41 +26,86 @@ class TRPNBackend:
 
         elif(command == '-'):
             if(len(self.stack) < 2):
-                print('unable to sub since stack is too short')
+                logging.getLogger(__name__).warning('unable to sub since stack is too short')
             else:
                 ultimate, penultimate = self.getLast2Items()
                 self.stack.append(penultimate - ultimate)
 
         elif(command == '*'):
             if(len(self.stack) < 2):
-                print('unable to mul since stack is too short')
+                logging.getLogger(__name__).warning('unable to mul since stack is too short')
             else:
                 ultimate, penultimate = self.getLast2Items()
                 self.stack.append(penultimate * ultimate)
 
         elif(command == '/'):
             if(len(self.stack) < 2):
-                print('unable to div since stack is too short')
+                logging.getLogger(__name__).warning('unable to div since stack is too short')
             else:
                 ultimate, penultimate = self.getLast2Items()
                 self.stack.append(penultimate / ultimate)
 
         elif(command == '^'):
             if(len(self.stack) < 2):
-                print('unable to power since stack is too short')
+                logging.getLogger(__name__).warning('unable to power since stack is too short')
             else:
                 ultimate, penultimate = self.getLast2Items()
                 self.stack.append(math.pow(penultimate, ultimate))
         
         elif(command == '%'):
             if(len(self.stack) < 2):
-                print('unable to mod since stack is too short')
+                logging.getLogger(__name__).warning('unable to mod since stack is too short')
             else:
                 ultimate, penultimate = self.getLast2Items()
                 self.stack.append(penultimate % ultimate)
+        
+        elif(command == 'e'):
+            self.stack.append(math.e)
+        
+        elif(command == 'p'):
+            self.stack.append(math.pi)
+        
+        elif(command == 'l'):
+            if(len(self.stack) < 1):
+                logging.getLogger(__name__).warning('unable to ln since stack is too short')
+            else:
+                a = self.stack.pop()
+                if(a<=0):
+                    self.stack.append(a)
+                    logging.getLogger(__name__).warning("can't ln <= 0")
+                else:
+                    self.stack.append(math.log(a, math.e))
+        
+        elif(command == 'r'):
+            if(len(self.stack) < 1):
+                logging.getLogger(__name__).warning('unable to random since stack too short')
+            else:
+                a = self.stack.pop()
+                if(a == 0):
+                    self.stack.append(a)
+                    logging.getLogger(__name__).warning("can't random from 0")
+                else:
+                    self.stack.append(random.randrange(int(a)))
+        
+        elif(command == 's'):
+            if(len(self.stack) < 1):
+                logging.getLogger(__name__).warning('unable to sin since stack too short')
+            else:
+                self.stack.append(math.sin(self.stack.pop()))
+        
+        elif(command == 'c'):
+            if(len(self.stack) < 1):
+                logging.getLogger(__name__).warning('unable to cos since stack too short')
+            else:
+                self.stack.append(math.cos(self.stack.pop()))
+        
+        elif(command == 't'):
+            if(len(self.stack) < 1):
+                logging.getLogger(__name__).warning('unable to tan since stack too short')
+            else:
+                self.stack.append(math.tan(self.stack.pop()))
 
-        elif(command == 'q'):
-            sys.exit(0)
+
 
     def pushToStack(self, newVal):
         if(type(newVal) == float):
@@ -75,7 +123,12 @@ while True:
         s.pushToStack(commandFloat)
     except:
         #print("command not in float format. attempting to process as a command:  ")
-        s.processCommand(command)
+        if(command == 'q'):
+            sys.exit(0)
+        elif(command == '.'):
+            print(s.popFromBOS())
+        else:
+            s.processCommand(command)
 
     s.printStack()
     
